16b86772984c6f998e373997ac358abc8a2652af
joyceliao <joyliao07@gmail.com>
2018-07-10 15:11:42 -0700

    work in progress lab2

---
+++ js/app.js
@@ -0,0 +1,67 @@
+alert("Do you want to know more about Joyce? She is a very cool developer!")
+alert("Here are some fun facts about Joyce!")
+
+var answerLanguage = prompt('Joyce is proficient in oral and written Chinese-English translation - T or F?');
+console.log('answerLanguage', answerLanguage);
+if (answerLanguage.toUpperCase() === 'T') {
+    alert('Yes, that\'s correct!');
+  
+  } else if (answerLanguage.toUpperCase() === 'F') {
+    alert('No, that\'s incorrect.');
+  
+  } else {
+    alert('That was not the answer I wanted... Try T or F next time.');
+  }
+
+
+
+var answerWork = prompt('Joyce had 10 years of experience working in finance and accounting – T or F?');
+console.log('answerWork', answerWork);
+if (answerWork.toUpperCase() === 'T') {
+    alert('Yes, that\'s correct!');
+  
+  } else if (answerWork.toUpperCase() === 'F') {
+    alert('No, that\'s incorrect.');
+  
+  } else {
+    alert('That was not the answer I wanted... Try T or F next time.');
+  }
+
+
+var answerConstellation = prompt('Joyce\'s zodiacal constellation is Cancer – T or F?');
+console.log('answerConstellation', answerConstellation);
+if (answerConstellation.toUpperCase() === 'T') {
+    alert('Yes, that\'s correct!');
+  
+  } else if (answerConstellation.toUpperCase() === 'F') {
+    alert('No, that\'s incorrect.');
+  
+  } else {
+    alert('That was not the answer I wanted... Try T or F next time.');
+  }
+
+var answerHobby = prompt('Joyce makes dance choreography – T or F?');
+console.log('answerHobby', answerHobby);
+if (answerHobby.toUpperCase() === 'T') {
+    alert('Yes, that\'s correct!');
+  
+  } else if (answerHobby.toUpperCase() === 'F') {
+    alert('No, that\'s incorrect.');
+  
+  } else {
+    alert('That was not the answer I wanted... Try T or F next time.');
+  }
+
+var answerFood = prompt('Joyce loves eating rice dumplings wrapped in bamboo leaves in Dragon Festival – T or F?');
+console.log('answerFood', answerFood);
+if (answerFood.toUpperCase() === 'T') {
+    alert('Yes, that\'s correct!');
+  
+  } else if (answerFood.toUpperCase() === 'F') {
+    alert('No, that\'s incorrect.');
+  
+  } else {
+    alert('That was not the answer I wanted... Try T or F next time.');
+  }
+
+
